b10bda6ba7ad83a0b438dd6cbce4e1f1a39beb0a
Eric Smith <genericmoniker@users.noreply.github.com>
2012-01-24 03:30:38 +0000

    Added (untested) deploy target. Removed obsolete package.bat.

--- AlertCamera.gadget/package.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Packages the gadget using 7zip
-if exist AlertCamera.gadget del AlertCamera.gadget
-C:\Tools\7zip\7za.exe a -tzip AlertCamera.gadget * -r -x!.svn -x!.komodotools -x!*.komodoproject -x!package.bat -x!readme.txt -x!version.json
